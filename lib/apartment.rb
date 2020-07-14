60f7c8b032447f03123c171785bbdce55fd979c3
Leah Riffell <leah.riffell@gmail.com>
2020-07-14 09:11:55 -0600

    Add initialize method for apartment class

---
+++ lib/apartment.rb
@@ -0,0 +1,9 @@
+class Apartment
+
+  def initialize(attributes = {})
+    @number = attributes[:number]
+    @monthly_rent = attributes[:monthly_rent]
+    @bathrooms = attributes[:bathrooms]
+    @bedrooms = attributes[:bedrooms]
+  end
+end
