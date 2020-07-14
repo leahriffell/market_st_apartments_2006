780c6015ada565b512c34efd4707c376b5b14492
Leah Riffell <leah.riffell@gmail.com>
2020-07-14 09:14:02 -0600

    Add test to ensure apartment has readable attributes

--- test/apartment_test.rb
+++ test/apartment_test.rb
@@ -9,4 +9,13 @@ class ApartmentTest < MiniTest::Test
 
     assert_instance_of Apartment, unit1
   end
+
+  def test_it_has_readable_attributes
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+
+    assert_equal "A1", unit1.number
+    assert_equal 1200, unit1.monthly_rent
+    assert_equal 1, unit1.bathrooms
+    assert_equal 1, unit1.bedrooms
+  end
 end
